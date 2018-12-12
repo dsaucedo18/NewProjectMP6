60bd21a74b1b054ffeddde349b193d9ae073155b
dsauce5 <dsauce5@illinois.edu>
2018-12-11 23:13:04 -0600

    First work done

---
+++ app/src/main/java/com/example/danny_mac/finalprojectcs/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.danny_mac.finalprojectcs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * Yeeer
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
